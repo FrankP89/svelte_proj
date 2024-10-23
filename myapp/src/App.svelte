d557803489de09994ac2bb0389141bd8a63bebcb
FrankP89 <walter.pintor@gmx.com>
2024-10-23 16:27:12 +0800

    First commit

---
+++ myapp/src/App.svelte
@@ -0,0 +1,15 @@
+<script>
+  // let name = 'svelte';
+  // let src = 'https://picsum.photos/200/300';
+  // let string = 'Some <strong>string</strong> with html'
+  import Counter from "./lib/Counter.svelte";
+</script>
+
+
+<Counter initialCount={3} maxCount={10}/>
+
+
+
+<style>
+
+</style>
---
+++ myapp/src/lib/Counter.svelte
@@ -0,0 +1,36 @@
+<script>
+  export let initialCount;
+  export let maxCount; // Always specify the value or the undefined state if not mentioned in the component.
+  let count = initialCount;
+  // let count2 = 0;
+  // $: {console.log(`You have clicked ${getTotal(count1, count2)} time(s)`);}
+  // $: string = `You have clicked ${getTotal(count1, count2)} time(s)`
+
+  function increment() {
+    if (count === maxCount) {
+      return;
+    }
+    count++;
+  }
+
+  // function increment2() {
+  //   count2++;
+  // }
+
+  function getTotal(x, y) {
+    return x + y
+  }
+
+
+</script>
+
+<button on:click={(increment)}>Clicks {count}</button>
+<!-- <button on:click={(increment2)}>Clicks {count2}</button> -->
+
+<!-- <h3>You have clicked {string} time(s)</h3> -->
+
+<style>
+  button{
+    padding: 15px 20 px;
+  }
+</style>
