5986c1fc51aa7ddeb8353298e5750046691512fb
FrankP89 <walter.pintor@gmx.com>
2024-10-27 15:44:26 +0800

    21. input bind

--- myapp/src/lib/TodoList.svelte
+++ myapp/src/lib/TodoList.svelte
@@ -1,11 +1,35 @@
 <script>
-export let todos = [];
+    import Button from "./Button.svelte";
 
+    export let todos = [];
+    let input;
+    let inputText = '';
+
+    function handleAddTodo() {
+        // console.log(input.value);
+        console.log(inputText);
+    }
 </script>
 
-<ui>
-    {#each todos as {id, title}, index (id)}
-        {@const number = index + 1}
-        <li>{number} - {title}</li>
-    {/each}
-</ui>
+<div class="todo-list-wrapper">
+    <ui>
+        {#each todos as { id, title }, index (id)}
+            {@const number = index + 1}
+            <li>{number} - {title}</li>
+        {/each}
+    </ui>
+    <form class="add-todo-form" 
+        on:submit|preventDefault={handleAddTodo} >
+        <!-- <input bind:this={input}/> -->
+        <!-- <input on:input={e => {
+            // console.log(e.currentTarget.value);
+            inputText = e.currentTarget.value;
+        }}
+        /> -->
+        <input 
+            bind:value={inputText}
+        />
+        <Button type="submit">Add</Button>
+    </form>
+
+</div>
